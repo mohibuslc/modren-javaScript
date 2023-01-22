5fe184f3a5de9c0b1ecf35aca6fbbc92052cfd69
mohibuslc <mohib_uslc@outlook.com>
2023-01-22 19:52:26 +0000

    Ternary Op

---
+++ ternaryOperator.js
@@ -0,0 +1,23 @@
+
+
+var age = 13; 
+
+
+
+// if - elas  statement alternative way learn 
+
+
+var person = (age >=18)? "Adult" : (age <= 10)? " Child" :" young"   ; 
+
+
+
+
+// if(age>=18){
+
+//     console.log("Adult")
+// }else{
+
+//     console.log(" Child")
+// }
+
+console.log(person)
